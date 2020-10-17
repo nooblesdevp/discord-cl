4ce5a132505ecdd1e6ffab31fdfd38236d47ec3c
nuzulzen <nuzulzen@gmail.com>
2020-10-17 11:10:17 +0700

    add social link and fix bug scroll

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 
 import "./Login.scss";
 import { auth, provider } from "../../firebase/firebase";
+import { Facebook, LinkedIn, Web } from "@material-ui/icons";
 
 function Login() {
   const signIn = () => {
@@ -11,13 +12,48 @@ function Login() {
   };
   return (
     <div className="login">
-      <div className="login__logo">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/sco/9/98/Discord_logo.svg"
-          alt=""
-        />
+      <div className="login__container">
+        <div className="login__logo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/sco/9/98/Discord_logo.svg"
+            alt=""
+          />
+        </div>
+        <div className="login__buttons">
+          <Button className="login__btn" onClick={signIn}>
+            Sign In
+          </Button>
+          <div className="login__socialLink">
+            <IconButton className="login__btnSocial">
+              <a
+                href="https://web.facebook.com/nuzulzen/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Facebook />
+              </a>
+            </IconButton>
+            <IconButton className="login__btnSocial">
+              <a
+                href="https://nuzul-zen-alfian.web.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Web />
+              </a>
+            </IconButton>
+            <IconButton className="login__btnSocial">
+              <a
+                href="https://www.linkedin.com/in/nuzul-zen-alfian-828297184/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedIn />
+              </a>
+            </IconButton>
+          </div>
+        </div>
       </div>
-      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 }
